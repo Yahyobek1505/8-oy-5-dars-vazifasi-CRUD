26d5708acf0d57c8fb9eabd3288177861e3c7ccc
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-05-18 10:08:37 +0500

    Card is done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,50 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
+import Form from "./components/Form";
 
-import './App.css'
-
-function App() {
-
-  return (
-    <>
-      <h1>Hello world</h1>
-    </>
-  )
+interface PhoneType {
+  id: string,
+  name: string,
+  description: string,
+  price: number,
+  status: string,
+  category_id: string,
+  created: string,
+  updated: string
 }
 
-export default App
+function App() {
+  const [data, setData] = useState<PhoneType[]>([])
+
+  async function getData(url:string) {
+    const response = await fetch (url)
+    const d = await response.json();
+    setData(d)
+  }
+
+
+  useEffect(() =>{
+    getData("https://auth-rg69.onrender.com/api/products/all")
+    
+
+  }, [])
+
+  return (
+    <div>
+      <div className="container  mx-auto">
+        <Form />
+
+        <div className="card-wrapper flex flex-wrap justify-center max-w-1/4 gap-8">
+         {
+          data.length && data.map((phone, index) =>{
+            return <Card key={index} data ={phone}/>
+          })
+         }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react"
+
+interface CardType {
+  data:{
+    id: string,
+  name: string,
+  description: string,
+  price: number,
+  status: string,
+  category_id: string,
+  created: string,
+  updated: string
+  }
+}
+
+const Card:FC<CardType> = (props) => {
+  return (
+    <>
+    <div className="card shadow-lg p-8 w-[18rem] bg-slate-100 cursor-pointer hover:translate-x-2 hover:translate-y-2 hover:shadow-2xl duration-500">
+    <h1>Nomi: {props.data.name}</h1>
+    <h3>izoh: {props.data.description}</h3>
+    <h3>nrxi: {props.data.price}</h3>
+    <h3>holati: {props.data.status}</h3>
+    </div>
+    </>
+  )
+}
+
+export default Card
