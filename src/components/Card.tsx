948d573540e9c9faeb75b06cac69096105eebeac
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-05-18 11:57:21 +0500

    Add form

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -16,8 +16,8 @@ interface CardType {
 const Card:FC<CardType> = (props) => {
   return (
     <>
-    <div className="card shadow-lg p-8 w-[18rem] bg-slate-100 cursor-pointer hover:translate-x-2 hover:translate-y-2 hover:shadow-2xl duration-500">
-    <h1>Nomi: {props.data.name}</h1>
+    <div className="card shadow-lg p-8 w-[18rem] rounded-lg border bg-slate-100 cursor-pointer hover:translate-x-2 hover:translate-y-2 hover:shadow-2xl duration-500">
+    <h1 className="text-xl font-semibold">Nomi: {props.data.name}</h1>
     <h3>izoh: {props.data.description}</h3>
     <h3>nrxi: {props.data.price}</h3>
     <h3>holati: {props.data.status}</h3>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,17 @@
-
 const Form = () => {
   return (
-    <div>
-      <h2>Form</h2>
-    </div>
-  )
-}
+    <>
+      <div className="form w-full my-4">
+        <h1 className="text-center font-semibold text-4xl pb-4">Add your products</h1>
+        <form className=" flex flex-col gap-4 bg-slate-300 max-w-[700px] mx-auto border p-4">
+          <input type="text" className="rounded-md indent-4 h-8" placeholder="Enter product name" />
+          <input type="number" className="rounded-md indent-4 h-8" placeholder="Enter product price"/>
+          <textarea name="desc" className="rounded-md indent-4" placeholder="Enter descripion"></textarea>
+          <button className="bg-blue-500 w-1/5 rounded-lg mx-auto">Send</button>
+        </form>
+      </div>
+    </>
+  );
+};
 
-export default Form
+export default Form;
